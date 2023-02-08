ad6e6332acc8aa555a84d66159b1657391609c58
kazzyan <kazzyan@hotmail.com>
2023-02-08 16:12:17 +0900

    Home tab / pull to refresh / infinite scroll

---
+++ components/ScreenLayout.js
@@ -0,0 +1,16 @@
+import { ActivityIndicator, View } from "react-native"
+
+export default ScreenLayout = ({ loading, children }) => {
+    return (
+        <View
+            style={{
+                backgroundColor: "black",
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            {loading ? <ActivityIndicator color="white" /> : children}
+        </View>
+    )
+}
--- screens/Feed.js
+++ screens/Feed.js
@@ -1,21 +1,54 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import { userLogOut } from "../apollo";
+import ScreenLayout from "../components/ScreenLayout";
+
+const SEE_COFFEEHOPS_QUERY = gql`
+    query seeCoffeeShops {
+        seeCoffeeShops {
+            id
+            name
+            user {
+                username
+            }
+            photos {
+                url
+            }
+            categories {
+                slug
+            }
+            createAt
+            updateAt
+        }
+    }
+`;
 
 export default function Feed() {
+    const [offset, setOffset] = useState(0);
+    const { data, loading, refetch, fetchMore } = useQuery(SEE_COFFEEHOPS_QUERY, {
+        variables: {
+            offset,
+        }
+    });
+
+    const [refreshing, setRefreshing] = useState(false);
+
+    const refresh = async () => {
+        setRefreshing(true);
+        await refetch();
+        setRefreshing(false)
+    }
+
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "black",
-            }}
-        >
-            <TouchableOpacity onPress={userLogOut}>
-                <Text style={{ color: "white" }} >
-                    This page is Feed!!
-                </Text>
-            </TouchableOpacity>
-        </View>
+        <ScreenLayout loading={loading}>
+            <FlatList
+                onEndReachedThreshold={0.3}
+                onEndReached={() => {alert("end reached")}}
+                refreshing={refreshing}
+                onRefresh={refresh}
+                data={data?.seeCoffeeShops}
+            />            
+        </ScreenLayout>
     );
 }
